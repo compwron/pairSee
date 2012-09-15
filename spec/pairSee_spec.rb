8eaca54137368f01d09b0e0ffd97dcbe84b1d12a
developer <lgoldste@thoughtworks.com>
2012-09-14 21:46:50 -0500

    &L: Move specs into subcontexts.

--- spec/pairSee_spec.rb
+++ spec/pairSee_spec.rb
@@ -28,84 +28,99 @@ describe PairSee do
       `rm -rf fake_git`
     end
 
-    it "contains counts for all pairs which have committed" do
-      subject.all_commits.should include "Person1, Person3: 2"
-      subject.all_commits.should include "Person1, Person2: 1"
+    describe "#all_commits" do
+      it "contains counts for all pairs which have committed" do
+        subject.all_commits.should include "Person1, Person3: 2"
+        subject.all_commits.should include "Person1, Person2: 1"
+      end
+
+      it "doesn't list counts for pairs who have no commits" do
+        subject.all_commits.should_not include "Person2, Person3: 0"
+      end
+
+      it "gets all commits which only have person1's name on them" do
+        subject.all_commits.should_not include "Person1: 0"
+        subject.all_commits.should include "Person3: 1"
+      end
+
+      it "sorts all by count" do
+        subject.all_commits.last.should include ": 2"
+        subject.all_commits.first.should include ": 1"
+      end
+
+      it "can see all commits since a passed-in date" do
+        all_commits_made_next_year = PairSee.new("fake_git", "spec/spec_config.yml", "2013-01-01")
+        all_commits_made_next_year.all_commits.size.should == 0
+      end
+
+      it "sees names with capitalization typos" do
+        subject.all_commits.should include "Person4, Person5: 1"
+      end
+
+      it "does not wrongfully exclude commits with 'merge' in the message from the count" do
+        subject.all_commits.should include "Person5: 1"
+      end
     end
 
-    it "doesn't list counts for pairs who have no commits" do
-      subject.all_commits.should_not include "Person2, Person3: 0"
+    describe "#commits_not_by_known_pair" do
+      it "prints a list of commits it did not connect with a name" do
+        extras = subject.commits_not_by_known_pair.map(&:to_s)
+        extras[0].should include "nameless" #hack to avoid regex right now
+        extras.should_not include "Person1, Person3: made cat"
+        extras.should_not include "Person1, Person3: 2"
+      end
+
+      it "does not include merge commits in the list of commits without dev names" do
+        subject.commits_not_by_known_pair.should_not include "Merge"
+      end
     end
 
-    it "gets all commits which only have person1's name on them" do
-      subject.all_commits.should_not include "Person1: 0"
-      subject.all_commits.should include "Person3: 1"
+    describe "#most_recent_commit_date" do
+      it "sees most recent commit by a pair" do
+        subject.most_recent_commit_date("Person4", "Person5").should == Time.parse(@current_date_string)
+        subject.most_recent_commit_date("Person4", "Person1").should == @never
+      end
     end
 
-    it "sorts all by count" do
-      subject.all_commits.last.should include ": 2"
-      subject.all_commits.first.should include ": 1"
+    describe "#all_most_recent_commits" do
+      it "gets most recent dates of all pair commits" do
+        subject.all_most_recent_commits.should include "Person1, Person3: #{@current_date_string}"
+        subject.all_most_recent_commits.should include "Person1, Person6: not yet"
+      end
+
+      it "gets most recent dates of all pair commits, sorted temporally" do
+        subject.all_most_recent_commits.first.should include "Person1, Person2: #{@current_date_string}"
+        subject.all_most_recent_commits.last.should include "not yet"
+      end
     end
 
-    it "can see all commits since a passed-in date" do
-      all_commits_made_next_year = PairSee.new("fake_git", "spec/spec_config.yml", "2013-01-01")
-      all_commits_made_next_year.all_commits.size.should == 0
+    describe "#unpaired_in_range" do
+      it "recommends pairs based on least recent active dev pair" do
+        subject.unpaired_in_range.should include "Person1, ActiveDev"
+        subject.unpaired_in_range.should_not include "Person1, Person2"
+      end
     end
 
-    it "sees names with capitalization typos" do
-      subject.all_commits.should include "Person4, Person5: 1"
+    describe "#active_devs" do
+      it "identifies active devs" do
+        active_devs = subject.active_devs("spec/spec_config.yml")
+        active_devs.should include "ActiveDev"
+        active_devs.should include "Person1" 
+        active_devs.should_not include "InactiveDev"
+      end
     end
 
-    it "prints a list of commits it did not connect with a name" do
-      extras = subject.commits_not_by_known_pair.map(&:to_s)
-      extras[0].should include "nameless" #hack to avoid regex right now
-      extras.should_not include "Person1, Person3: made cat"
-      extras.should_not include "Person1, Person3: 2"
+    describe "#is_active" do
+      it "is true when the dev is active" do
+        subject.is_active("ActiveDev").should == true
+        subject.is_active("InactiveDev").should == false
+      end
     end
 
-    it "does not include merge commits in the list of commits without dev names" do
-      subject.commits_not_by_known_pair.should_not include "Merge"
+    describe "#least_recent_pair" do
+      it "sees least recent pairing" do
+        subject.least_recent_pair.should include "Person1, Person2"
+        subject.least_recent_pair.should_not include "ActiveDev"
+      end
     end
-
-    it "does not wrongfully exclude commits with 'merge' in the message from the count" do
-      subject.all_commits.should include "Person5: 1"
-    end
-
-    it "sees most recent commit by a pair" do
-      subject.most_recent_commit_date("Person4", "Person5").should == Time.parse(@current_date_string)
-      subject.most_recent_commit_date("Person4", "Person1").should == @never
-    end
-
-    it "gets most recent dates of all pair commits" do
-      subject.all_most_recent_commits.should include "Person1, Person3: #{@current_date_string}"
-      subject.all_most_recent_commits.should include "Person1, Person6: not yet"
-    end
-
-    it "gets most recent dates of all pair commits, sorted temporally" do
-      subject.all_most_recent_commits.first.should include "Person1, Person2: #{@current_date_string}"
-      subject.all_most_recent_commits.last.should include "not yet"
-    end
-
-    it "should recommend pairs based on least recent active dev pair" do
-      subject.unpaired_in_range.should include "Person1, ActiveDev"
-      subject.unpaired_in_range.should_not include "Person1, Person2"
-    end
-
-    it "should identify active devs, i.e. has committed in the last two weeks" do
-      active_devs = subject.active_devs("spec/spec_config.yml")
-      active_devs.should include "ActiveDev"
-      active_devs.should include "Person1" 
-      active_devs.should_not include "InactiveDev"
-    end
-
-    it "knows whether dev is active" do
-      subject.is_active("ActiveDev").should == true
-      subject.is_active("InactiveDev").should == false
-    end
-
-    it "sees least recent pairing" do
-      subject.least_recent_pair.should include "Person1, Person2"
-      subject.least_recent_pair.should_not include "ActiveDev"
-    end
-
   end
