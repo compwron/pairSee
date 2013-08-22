e5abd6cdc446af9049cc154d58aaa6d607d72461
compwron <wordymusic@gmail.com>
2013-08-22 07:09:23 -0500

    adding parsing of committer id to name mappings in config file.

--- lib/svn_log_lines.rb
+++ lib/svn_log_lines.rb
@@ -1,18 +1,33 @@
 class SvnLogLines
   attr_reader :lines
 
-  def initialize(log_location, after_date)
+  def initialize(log_location, after_date, config)
     @lines = lines_from(log_location)
+    @committers = committer_mappings(config)
+  end
 
+  def committer_mappings(config)
+    config = YAML.load_file(config)
+    mappings = {}
+    config['committers'].split(",").map {|pairing|
+      pair = pairing.split(" ")
+       mappings.merge!({pair.first => pair.last})
+    }
+     mappings
   end
 
   def lines_from log_location
     lines = []
     File.open(log_location) do |infile|
       while (line = infile.gets)
-        lines << line
+        lines << LogLine.new(line)
       end
     end
     lines
+  end
+
+  def authored_by? *people
+    people.all? { |person| /#{person}/i =~ line }
+
   end
 end
--- spec/svn_log_lines_spec.rb
+++ spec/svn_log_lines_spec.rb
@@ -1,14 +1,38 @@
 require_relative "spec_helper"
 
 describe SvnLogLines do
-  let(:log_location) {'spec/fixtures/svn_log.txt'}
+  let(:log_location) { 'spec/fixtures/one_commit.txt' }
   let(:after_date) { '0-1-1' }
-  let(:log_lines) { SvnLogLines.new(log_location, after_date).lines }
+  let(:config) { 'spec/fixtures/config.yml' }
+  let(:log_lines) { SvnLogLines.new(log_location, after_date, config) }
 
   describe "lines" do
     let(:log_location) { 'spec/fixtures/one_commit.txt' }
+
     it "make one line from a file containing one line" do
-      log_lines.size.should == 1
+      log_lines.lines.size.should == 1
+    end
+
+    it "should see that line is authored by a line author" do
+      log_lines.lines.first.authored_by?("Alice").should be_true
+    end
+
+    it "should see that, in absence of line author, line is authored by the name mapped to commitID1" do
+      log_lines.lines.first.authored_by?("Alice").should be_true
+    end
+  end
+
+  describe "lines without authors" do
+    let(:log_location) { 'spec/fixtures/one_commit_without_author.txt' }
+
+    it "should see that, in absence of line author, line is authored by the name mapped to commitID1" do
+      #log_lines.lines.first.authored_by?("Alice").should be_true
+    end
+  end
+
+  describe "#committer_mappings" do
+    it "should see committers in config file" do
+      log_lines.committer_mappings(config).should == {"committerID1" => "Alice", "committerID2" => "Bob"}
     end
   end
 
