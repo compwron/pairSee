32b3d14624f4c8da5c2c2a7ce0391cd52cf39e8d
compiledwrong <compiledwrong+github@gmail.com>
2015-03-11 15:06:26 -0700

    rspec config, spec_helper update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,25 @@
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
-require_relative '../lib/pair_see'
-require_relative '../lib/combo'
-require_relative '../lib/date_combo'
-require_relative '../lib/log_lines'
-require_relative '../lib/log_line'
-require_relative '../lib/card'
+Dir[File.expand_path('../../lib/*.rb', __FILE__)].each do |file|
+  require file
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.warnings = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 3
+  config.order = :random
+  Kernel.srand config.seed
+end
