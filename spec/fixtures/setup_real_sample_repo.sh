07cc112e518781c98ceed8b51c6c452a9094f467
compwron <compiledwrong+github@gmail.com>
2015-03-11 18:08:12 -0700

    WIP manual test script

---
+++ spec/fixtures/setup_real_sample_repo.sh
@@ -0,0 +1,19 @@
+# intended to be run from pairSee root dir
+rm -rf spec/fixtures/example_usage_of_pairsee
+mkdir spec/fixtures/example_usage_of_pairsee
+cd spec/fixtures/example_usage_of_pairsee
+git init                                                          
+echo bar >> foo.txt ; git add -A ; git commit -m "noone foo"                       
+echo bar >> foo.txt ; git add -A ; git commit -m "Person1/Person3 [FOO-1] foo"     
+echo bar >> foo.txt ; git add -A ; git commit -m "Person2/Person3 [FOO-1] foo"     
+echo bar >> foo.txt ; git add -A ; git commit -m "Person1/Person2 [FOO-3] foo"     
+echo bar >> foo.txt ; git add -A ; git commit -m "Person1/Person3 [FOO-100] foo"   
+echo bar >> foo.txt ; git add -A ; git commit -m "Person1/Person3 [FOO-001] foo"   
+cd -
+pairsee -r spec/fixtures/example_usage_of_pairsee -c config/sample_config.yml
+pairsee -r spec/fixtures/example_usage_of_pairsee -c config/sample_config.yml -h
+pairsee -r spec/fixtures/example_usage_of_pairsee -c config/sample_config.yml -e
+pairsee -r spec/fixtures/example_usage_of_pairsee -c config/sample_config.yml -l
+pairsee -r spec/fixtures/example_usage_of_pairsee -c config/sample_config.yml -d
+pairsee -r spec/fixtures/example_usage_of_pairsee -c config/sample_config.yml -s
+# rm -rf spec/fixtures/example_usage_of_pairsee # comment this out in order to leave sample repo in place for later examination
