1ac33bdd53068c87525635ae5da9f0a4bb1f9017
Test User <user@example.com>
2017-06-11 15:50:30 -0700

    Fixing test data to allow multiple names per person - subseer

--- spec/sub_seer_spec.rb
+++ spec/sub_seer_spec.rb
@@ -1,32 +1,35 @@
 describe PairSee::CardsPerPerson do
 
   describe '#cards_per_person' do
-    subject { PairSee::CardsPerPerson.new(log_lines, options).cards_per_person }
-    let(:log_lines) { PairSee::LogLines.new([]) }
-    let(:options) { {names: []} }
+    subject {PairSee::CardsPerPerson.new(log_lines, card_prefix, people).cards_per_person}
+    let(:log_lines) {PairSee::LogLines.new([])}
+    let(:people) {[]}
+    let(:card_prefix) {}
+
     it "should see no cards for a person when there are no cards or people" do
       expect(subject).to eq([])
     end
 
     describe "with people but no cards" do
-      let(:options) { {names: ["person1", "person2"]} }
+      let(:people) {[PairSee::Person.new(["name"])]}
       it "should see no cards for a person" do
         expect(subject).to eq([])
       end
     end
 
     describe "with cards but no people" do
-      let(:log_lines) { [PairSee::LogLines.new([PairSee::LogLine.new("msg1"), PairSee::LogLine.new("msg2")])] }
+      let(:log_lines) {[PairSee::LogLines.new([PairSee::LogLine.new("msg1"), PairSee::LogLine.new("msg2")])]}
       it "should see no cards for a person" do
         expect(subject).to eq([])
       end
     end
 
     describe "with cards and people" do
-      let(:log_lines) { PairSee::LogLines.new(
+      let(:log_lines) {PairSee::LogLines.new(
           [PairSee::LogLine.new("FOO-1: person1 person2 msg1"),
-           PairSee::LogLine.new("FOO-2: person1 msg2")]) }
-      let(:options) { {names: ["person1", "person2"], card_prefix: ["FOO-"]} }
+           PairSee::LogLine.new("FOO-2: person1 msg2")])}
+      let(:people) {[PairSee::Person.new(["person1"]), PairSee::Person.new(["person2"])]}
+      let(:card_prefix) {["FOO-"]}
       it "should see cards for people" do
         expect(subject).to eq(["person2: [1 cards] 1", "person1: [2 cards] 1, 2"]);
       end
