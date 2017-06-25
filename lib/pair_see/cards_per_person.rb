2002ccaa798c5167ac0f0d60e270c72f9f6f08f0
Test User <user@example.com>
2017-06-25 13:53:32 +0800

    refactoring

--- lib/pair_see/cards_per_person.rb
+++ lib/pair_see/cards_per_person.rb
@@ -9,19 +9,35 @@ module PairSee
     end
 
     def cards_per_person
-      all = Hash[people.map { |key, _| [key, []] }]
+      all = people_hash
+      populate_card_numbers(all)
+      unique_cards_per_person(all)
+      sort_by_cards_count(all)
+    end
 
-      @log_lines.each do |log_line| # loop through the biggest list only once
-        all.each do |person, _|
-          all[person] << log_line.card_number(@card_prefix) if log_line.authored_by? person
-        end
-      end
-
-      all.each { |_, card_names| card_names.uniq! }
-      all.sort_by { |_, card_names| card_names.count }.map do |person, card_names|
+    def sort_by_cards_count(all)
+      all.sort_by {|_, card_names| card_names.count}.map do |person, card_names|
         sorted = card_names.compact.sort_by(&:to_i)
         "#{person}: [#{card_names.size} cards] #{sorted.join(', ')}"
       end
+    end
+
+    def unique_cards_per_person(all)
+      all.each {|_, card_names| card_names.uniq!}
+    end
+
+    def populate_card_numbers(all)
+      @log_lines.each do |log_line| # loop through the biggest list only once
+        all.each do |person, _|
+          if log_line.authored_by? person then
+            all[person] << log_line.card_number(@card_prefix)
+          end
+        end
+      end
+    end
+
+    def people_hash
+      Hash[people.map {|key, _| [key, []]}]
     end
 
     def _active(people)
